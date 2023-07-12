dda1ed8512f600f4bb3afad16b3a99e7fbf65c38
joãoguilherme <112426149+JG310712@users.noreply.github.com>
2023-07-12 15:29:30 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,55 @@
+img = "";
+noseX = 0;
+noseY = 0;
+marioX = 325;
+marioY = 325;
+
+function preload(){
+    world_start = loadSound("worlt_start.wav");
+    setSprites();
+    MarioAnimation();
+}
+
+function setup(){
+    canvas = createCanvas(1240,336);
+    canvas.parent('canvas');
+
+    instializeInSetup(mario);
+
+    video = createCapture(VIDEO);
+    video.size(800,400);
+    video.parent('game_console');
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+    console.log('pose', gotPoses);
+}
+
+function draw(){
+    game();
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+    }
+}
+
+GameStatus = "";
+
+function game(){
+    instializeInDraw();
+    moveEnvironment(mario);
+    drawSprites();
+    console.log("noseX =" + noseX + ",noseY =" + noseY);
+}
+
+function startGame(){
+    GameStatus = "start";
+    document.getElementById("status").innerHTML = "O jogo está carregando";
+}
